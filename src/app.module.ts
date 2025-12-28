c9aed6357d39d5e9115ff45f6244206d25f36618
Thalyson Menezes <thalyson-luan@hotmail.com>
2025-12-28 17:46:34 -0300

    refactor(src): remoção de arquivos iniciais do nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PaymentsModule } from '@/modules/payments/payments.module';
 import { ConfigModule } from '@nestjs/config';
 import { DrizzleModule } from '@/infra/database/postgres-drizzle/drizzle-connection.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), DrizzleModule, PaymentsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
